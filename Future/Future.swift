61a8a4e5659afba3d76b65337bde88eb0af818eb
Elviro Rocca <elviro.rocca@facile.it>
2015-09-24 11:05:47 +0200

    modernized

--- Future/Future.swift
+++ Future/Future.swift
@@ -1,189 +1,202 @@
 /// A 'Future' holds a result: Either<E,T>? that can be, eventually, available at a certain time
 /// Once the Future is completed, its value can't change any further
-/// The Future state could be 'not completed', thus result == nil, or 'completed', thus result != nil and can be .Right or .Left
-/// Various callback methods can inform client that a Future is in state 'completed'
+/// The Future state could be '.Working', or '.Done' with a result that can be .Right or .Left
+/// Various callback methods can inform client that a Future is in state '.Done'
 
 import Foundation
 import Queue
 import Swiftz
 
+public enum FutureState<E,T>
+{
+  case Working
+  case Done(Either<E,T>)
+}
+
 ///MARK: - basic definitions
-final public class Future<T,E> {
-    
-    ///  'result' is an exposed, non-mutable, computed property that refers to the invisible, private var result_internal; this construct is necessary because 'result' should be able to mutate over time, but shouldn't be mutated by clients
-    private var result_internal: Either<E,T>? = nil
-    public var result: Either<E,T>? {
-        return result_internal
-    }
-    
-    /// useful typealias to represent a generic task that returns a Result
-    public typealias Task = () -> Either<E,T>
-    
-    /// the designated init method accepts a Task, and a Queue to execute the task
-    public init(queue: Queue, _ task: Task?) {
-        if let actualTask = task {
-            queue.async { () -> () in
-                let result = actualTask()
-                Queue.main.async { () -> () in
-                    self.complete(result)
-                    return
-                }
-            }
+final public class Future<E,T>
+{
+  /// 'state' is not exposed: clients should communicate with a Future with onComplete, onSuccess and onFailure methods;
+  /// that means that no client should expect a synchronous execution for 'on' methods
+  private var state = FutureState<E,T>.Working
+  
+  /// useful typealias to represent a generic task that returns a Either
+  public typealias Task = () -> Either<E,T>
+  
+  /// the designated init method accepts a Task, and a Queue to execute the task
+  public init (queue: Queue, _ optionalTask: Task?) {
+    if let
+      task = optionalTask
+    {
+      queue.async {
+        let result = task()
+        Queue.main.async {
+          self.complete(result)
         }
+      }
+    }
+  }
+  
+  /// a convenience init method only accepts the task, and considers the generic global queue
+  public convenience init (_ task: Task?)
+  {
+    self.init(queue: Queue.global, task)
+  }
+  
+  /// a convenience empty init method doesn't associate any predefined task to the future, and it's used by Promise to manually complete the Future
+  public convenience init()
+  {
+    self.init(nil)
+  }
+  
+  /// some typealiases to conveniently define the various kinds of callbacks
+  public typealias CompletionCallback = Either<E,T> -> ()
+  public typealias SuccessCallback = T -> ()
+  public typealias FailureCallback = E -> ()
+  
+  /// all callbacks are actually wrapped in a single kind of internal callback, and then are collected in a single array for execution
+  private typealias CallbackInternal = Future<E,T> -> ()
+  private var callbacks: [CallbackInternal] = Array<CallbackInternal>()
+  func runCallbacks()
+  {
+    /// all callbacks are collected in an immutable array before calling, and the mutable version is cleaned
+    let currentCallbacks = callbacks.map { $0 }
+    callbacks.removeAll()
+    for callback in currentCallbacks
+    {
+      callback(self)
     }
     
-    /// a convenience init method only accepts the task, and considers the generic global queue
-    public convenience init(_ task: Task?) {
-        self.init(queue: Queue.global, task)
+    /// if there's still some callback to call (because during the for cycle some callback was added to 'callbacks'), the function is called again
+    if callbacks.count > 0
+    {
+      runCallbacks()
     }
-    
-    /// a convenience empty init method doesn't associate any predefined task to the future, and it's used by Promise to manually complete the Future
-    public convenience init() {
-        self.init(nil)
+  }
+  
+  /// all callbacks are executed on a background, serial queue
+  let callbackExecutionQueue = Queue(dispatch_queue_create("callbackExecutionQueue", DISPATCH_QUEUE_SERIAL))
+  
+  /// the 'onComplete' method creates an internal callback and adds it to the callbacks array, or directly runs the callback if '.Done'
+  public func onComplete (callback: CompletionCallback) -> Future
+  {
+    switch state
+    {
+    case .Done(let result):
+      callback(result)
+    case .Working:
+      callbacks.append { $0.onComplete(callback) }
     }
-    
-    /// convenience computed property, to check is the current state is 'completed' or not
-    public var isCompleted: Bool {
-        return self.result_internal != nil
+    return self
+  }
+  
+  /// the 'onSuccess' and 'onFailure' methods are convenience methods that call the 'onComplete' method with a CompletionCallback that actually calls the input callback exclusively if Either is respectively .Right or .Left
+  public func onSuccess (callback: SuccessCallback) -> Future
+  {
+    onComplete { result in
+      switch result
+      {
+      case .Right(let value):
+        callback(value)
+      case .Left:
+        break
+      }
     }
-    
-    /// some typealiases to conveniently define the various kinds of callbacks
-    public typealias CompletionCallback = Either<E,T> -> ()
-    public typealias SuccessCallback = T -> ()
-    public typealias FailureCallback = E -> ()
-    
-    /// all callbacks are actually wrapped in a single kind of internal callback, and then are collected in a single array for execution
-    private typealias CallbackInternal = (future: Future<T,E>) -> ()
-    private var callbacks: [CallbackInternal] = Array<CallbackInternal>()
-    func runCallbacks() {
-        /// all callbacks are collected in an immutable array before calling, and the mutable version is cleaned
-        let currentCallbacks = callbacks.map { $0 }
-        callbacks.removeAll()
-        for callback in currentCallbacks {
-            callback(future: self)
-        }
-        
-        /// if there's still some callback to call (because during the for cycle some callback was added to 'callbacks'), the function is called again
-        if (callbacks.count > 0) {
-            runCallbacks()
-        }
+    return self
+  }
+  
+  public func onFailure (callback: FailureCallback) -> Future
+  {
+    onComplete { result in
+      switch result
+      {
+      case .Left(let value):
+        callback(value)
+      default:
+        break
+      }
     }
-    
-    /// all callbacks are executed on a background, serial queue
-    let callbackExecutionQueue = Queue(dispatch_queue_create("callbackExecutionQueue", DISPATCH_QUEUE_SERIAL))
-    
-    /// the 'onComplete' method creates an internal callback and adds it to the callbacks array, or directly runs the callback if 'isCompleted == true'
-    public func onComplete(callback: CompletionCallback) -> Future {
-        if let result = result {
-            callback(result)
-        }
-        else {
-            let callbackInternal: CallbackInternal = { future in
-                future.callbackExecutionQueue.sync {
-                    if let result = future.result {
-                        callback(result)
-                    }
-                }
-            }
-            callbacks.append(callbackInternal)
-        }
-        return self
-    }
-    
-    /// the 'onSuccess' and 'onFailure' methods are convenience methods that call the 'onComplete' method with a CompletionCallback that actually calls the input callback exclusively if Result is respectively .Right or .Left
-    public func onSuccess(callback: SuccessCallback) -> Future {
-        onComplete { result in
-            switch result {
-            case .Right(let value):
-                callback(value)
-            default:
-                break
-            }
-        }
-        return self
-    }
-    public func onFailure(callback: FailureCallback) -> Future {
-        onComplete { result in
-            switch result {
-            case .Left(let value):
-                callback(value)
-            default:
-                break
-            }
-        }
-        return self
-    }
+    return self
+  }
 }
 
-extension Future {
-    /// the 'complete' method completes the future by assigning a value to 'result' and then running callbacks
-    /// the method returns a Bool because the future will be completed excusively if its state is 'not completed' (that is, self.result_internal == nil)
-    /// the method is internal: a Future provider should use the Promise interface to complete a Future
-    func complete(result: Either<E,T>) -> Bool {
-        if isCompleted {
-            return false
-        }
-        else {
-            result_internal = result
-            runCallbacks()
-            return true
-        }
+extension Future
+{
+  /// the 'complete' internal method completes the future by assigning a value to 'state' and then running callbacks
+  /// the method returns a Bool because the future will be completed excusively if its state is '.Working'
+  /// the method is internal: a Future provider should use the Promise interface to complete a Future
+  func complete (result: Either<E,T>) -> Bool
+  {
+    switch state
+    {
+    case .Working:
+      state = .Done(result)
+      runCallbacks()
+      return true
+    case .Done:
+      return false
     }
+  }
 }
 
-extension Future {
-
-    public func map <U> (change: T -> U) -> Future<U,E> {
-        let newFuture = Future<U,E>()
-        onComplete { result in
-            switch result {
-            case .Right(let value):
-                newFuture.complete(Either.Right(change(value)))
-            case .Left(let error):
-                newFuture.complete(Either.Left(error))
-            }
-        }
-        return newFuture
+extension Future
+{
+  public func map <U> (change: T -> U) -> Future<E,U>
+  {
+    let newFuture = Future<E,U>()
+    onComplete {
+      switch $0
+      {
+      case .Right(let value):
+        newFuture.complete § Either.Right § change § value
+      case .Left(let error):
+        newFuture.complete § Either.Left § error
+      }
     }
-    
-    public func flatMap <U> (change: T -> Future<U,E>) -> Future<U,E> {
-        let newFuture = Future<U,E>()
-        onComplete { result in
-            switch result {
-            case .Right(let value):
-                change(value).onComplete { newFuture.complete($0) }
-            case .Left(let error):
-                newFuture.complete(Either.Left(error))
-            }
-        }
-        return newFuture
+    return newFuture
+  }
+  
+  public func flatMap <U> (change: T -> Future<E,U>) -> Future<E,U>
+  {
+    let newFuture = Future<E,U>()
+    onComplete {
+      switch $0
+      {
+      case .Right(let value):
+        change(value).onComplete { newFuture.complete($0) }
+      case .Left(let error):
+        newFuture.complete § Either.Left § error
+      }
     }
-    
-    public func zip <U> (other: Future<U,E>) -> Future<(T,U),E> {
-        return flatMap { value -> Future<(T,U),E> in
-            return other.map { otherValue in
-                return (value, otherValue)
-            }
-        }
-    }
+    return newFuture
+  }
+  
+  public func zip <U> (other: Future<E,U>) -> Future<E,(T,U)>
+  {
+    return flatMap { value in other.map { otherValue in (value, otherValue) } }
+  }
 }
 
 ///MARK: - 'Applicative' and 'Monad' definitions
 
-public func pure <A,E> (value: A) -> Future<A,E> {
-    return fulfilled(value)
+public func pure <E,A> (value: A) -> Future<E,A>
+{
+  let future = Future<E,A>()
+  future.complete(Either<E,A>.Right(value))
+  return future
 }
 
-public func <*> <A,B,E> (lhs: Future<A->B,E>, rhs: Future<A,E>) -> Future<B,E> {
-    return lhs.zip(rhs).map { change, value in
-        return change(value)
-    }
+public func <*> <E,A,B> (lhs: Future<E,A->B>, rhs: Future<E,A>) -> Future<E,B>
+{
+  return lhs.zip(rhs).map { $0($1) }
 }
 
-public func <^> <A,B,E> (lhs: A->B, rhs: Future<A,E>) -> Future<B,E> {
-    return pure(lhs) <*> rhs
+public func <^> <E,A,B> (lhs: A->B, rhs: Future<E,A>) -> Future<E,B>
+{
+  return pure(lhs) <*> rhs
 }
 
-public func >>- <T,U,E> (lhs: Future<T,E>, rhs: T->Future<U,E>) -> Future<U,E> {
-    return lhs.flatMap(rhs)
+public func >>- <E,A,B> (lhs: Future<E,A>, rhs: A->Future<E,B>) -> Future<E,B>
+{
+  return lhs.flatMap(rhs)
 }
--- Future/Promise.swift
+++ Future/Promise.swift
@@ -4,42 +4,44 @@
 import Foundation
 import Swiftz
 
-public class Promise<T,E> {
-    public let future: Future<T,E>
-    
-    public init() {
-        self.future = Future<T,E>()
-    }
-    
-    public func complete(result: Either<E,T>) {
-        future.complete(result)
-    }
-    
-    /// the method 'completeWith' completes the current future with the result obtained by the completion of another Future
-    public func completeWith(future: Future<T,E>) {
-        future.onComplete { result in
-            self.complete(result)
-        }
-    }
+public class Promise<E,T>
+{
+  public let future: Future<E,T>
+  
+  public init()
+  {
+    self.future = Future<E,T>()
+  }
+  
+  public func complete (result: Either<E,T>) -> Promise<E,T>
+  {
+    future.complete(result)
+    return self
+  }
+  
+  /// the method 'completeWith' completes the current future with the result obtained by the completion of another Future
+  public func completeWith (future: Future<E,T>) -> Promise<E,T>
+  {
+    future.onComplete { [unowned self] result in self.complete(result) }
+    return self
+  }
+  
+  /// the func 'completed' returns an already completed Future: useful for using functions that require Futures as input in situations in which the data is ready
+  public static func completed (result: Either<E,T>) -> Future<E,T>
+  {
+    return Promise<E,T>().complete(result).future
+  }
+  
+  /// the func 'fulfilled' already completed Future that contains a successful Result
+  public static func fulfilled (value: T) -> Future<E,T>
+  {
+    return Promise<E,T>().complete(Either<E,T>.Right(value)).future
+  }
+  
+  /// 'unfulfilled' does the same with an error
+  public static func unfulfilled <E,T> (error: E) -> Future<E,T>
+  {
+    return Promise<E,T>().complete(Either<E,T>.Left(error)).future
+  }
 }
 
-/// the func 'completed' returns an already completed Future: useful for using functions that require Futures as input in situations in which the data is ready
-public func completed <T,E> (result: Either<E,T>) -> Future<T,E> {
-    let promise = Promise<T,E>()
-    promise.complete(result)
-    return promise.future
-}
-
-/// the func 'fulfilled' already completed Future that contains a successful Result
-public func fulfilled <T,E> (value: T) -> Future<T,E> {
-    let promise = Promise<T,E>()
-    promise.complete(Either<E,T>.Right(value))
-    return promise.future
-}
-
-/// 'unfulfilled' does the same with an error
-public func unfulfilled <T,E> (error: E) -> Future<T,E> {
-    let promise = Promise<T,E>()
-    promise.complete(Either.Left(error))
-    return promise.future
-}
